b0897140597dd9ba934fd791b4448ae962526b2b
lookatgithub <puyike@gmail.com>
2020-03-12 15:45:06 -0400

    Deliverable 2 version

---
+++ src/ca/sheridancollege/project/GamePlayer.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.project;
+import ca.sheridancollege.project.Game;
+
+/**
+ *
+ * @author Gon
+ */
+public class GamePlayer{
+    public static void main(String[] args){
+        Game g1 = new Game("Blackjack");
+    }
+}
